29f748f801b7ebfb9b112617352d4d77376fdf2a
herrpfarrer <97227216+herrpfarrer@users.noreply.github.com>
2022-01-06 11:50:28 +0100

    Upload Modul Dateien

---
+++ mod_etliste.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Modul Evangelische Termine Liste
+ * 
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+
+// Include the syndicate functions only once
+require_once dirname(__FILE__) . '/helper.php';
+$liste = modETListeHelper::getListe($params);
+
+// Modulklassen-Suffix
+$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
+
+require JModuleHelper::getLayoutPath('mod_etliste');
