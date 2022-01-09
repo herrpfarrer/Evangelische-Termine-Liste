b8373d8bcc0b353b585cdbf951bf810aa19879c9
herrpfarrer <herr_pfarrer@posteo.de>
2022-01-09 18:18:03 +0100

    Ordnerstruktur an J4-Konvention angepasst

--- mod_etliste.php
+++ mod_etliste.php
@@ -8,7 +8,7 @@
 defined('_JEXEC') or die;
 
 // Include the syndicate functions only once
-require_once dirname(__FILE__) . '/helper.php';
+require_once dirname(__FILE__) . '/Helper/GetListeHelper.php';
 $liste = modETListeHelper::getListe($params);
 
 // Modulklassen-Suffix
