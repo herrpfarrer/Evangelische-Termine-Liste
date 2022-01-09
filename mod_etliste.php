270ce1f26c30f1bde23033c5d720a3f0b82cf075
herrpfarrer <herr_pfarrer@posteo.de>
2022-01-09 18:20:56 +0100

    Ordnerstruktur an J4-Konvention angepasst - Fehlerkorrektur

--- mod_etliste.php
+++ mod_etliste.php
@@ -9,7 +9,7 @@ defined('_JEXEC') or die;
 
 // Include the syndicate functions only once
 require_once dirname(__FILE__) . '/Helper/GetListeHelper.php';
-$liste = modETListeHelper::getListe($params);
+$liste = GetListeHelper::getListe($params);
 
 // Modulklassen-Suffix
 $moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
