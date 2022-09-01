3f59ffb1d9c7b1c70b4bf609406b43a813584562
herrpfarrer <97227216+herrpfarrer@users.noreply.github.com>
2022-09-01 21:04:01 +0200

    Version 2.0.4 + Feature Überschriften ausblenden
    1. Fügt unter Layout-Optionen das Feld "Zeige Überschrift?" hinzu. Wird "Nein, ausblenden" ausgewählt, wird die entsprechende Node "etliste_headline" aus dem HTML-Dom entfernt
    2. Bisherige Standard-VeranstalterID war ID der KG Wonsees. Die MusterID 3 bietet aber mehr Einträge und wird darum zur neuen StandardID
    3. Neue Version

--- Helper/GetListeHelper.php
+++ Helper/GetListeHelper.php
@@ -105,7 +105,7 @@ class GetListeHelper
 		// Für die Dekanatsausgabe geben Sie als Veranstalter-ID 'all' ein.
 		$veranstalterID = $params->get('veranstalterid');
 		if($veranstalterID == ''){
-			$veranstalterID = '2220';
+			$veranstalterID = '3';
 		}
 
 		// Dekanats-Nummer
@@ -290,6 +290,13 @@ class GetListeHelper
 		//  C2. Layouteinstellungen für Übersichtsseite/Liste
 		// = = = = = = = = = = = = = = = = = = = = = = = = = = 
 		
+		// Zeige Überschrift
+		// Soll die Überschrift angezeigt werden?
+		$showheadline = $params->get('showheadline');
+		if ($showheadline == '' ){
+			$showheadline = 'true';
+		}
+
 		// Zeige Highlights?
 		// Soll die Eingabe- bzw. Filtermethode 'Highlights' angezeigt werden?
 		$showhighlight = $params->get('showhighlight');
@@ -621,8 +628,16 @@ class GetListeHelper
 					$inputfield->setAttribute('value', $_REQUEST['et_q']);
 				}
 
+				//  B2. Blende Überschift aus, falls gewünscht
+				// = = = = = = = = = = = = = = = = = = = = = = = = =
+				if ($showheadline == 'false'){
+					$div_etliste_headline = $dom->getElementById('etliste_headline');
+					if (isset($div_etliste_headline)){
+						$div_etliste_headline->parentNode->removeChild($div_etliste_headline);
+					}
+				}
 								
-				//  B2. Zeige nur die ausgewählten Eingabe- und Filtermethoden an
+				//  B3. Zeige nur die ausgewählten Eingabe- und Filtermethoden an
 				// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 				
 				// Falls gar keine Filtermethode angezeigt werden soll, lösche das übergeordnete div aller Filter ('etliste_filter_container')
@@ -694,7 +709,7 @@ class GetListeHelper
 				}
 
 
-				//  B3. Ergänze Kategorie 'Außer Gottesdienste'
+				//  B4. Ergänze Kategorie 'Außer Gottesdienste'
 				// = = = = = = = = = = = = = = = = = = = = = = =
 				$eventtypenode = $dom->getElementById('eventtype');
 				if (isset($eventtypenode)){
